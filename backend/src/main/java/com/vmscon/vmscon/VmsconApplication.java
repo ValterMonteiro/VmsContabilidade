c9654fc9afb3542fcadef3c5f487013e79f39e6f
Valter Monteiro <valtinms@gmail.com>
2021-09-07 18:28:58 -0400

    Project created

---
+++ backend/src/main/java/com/vmscon/vmscon/VmsconApplication.java
@@ -0,0 +1,13 @@
+package com.vmscon.vmscon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsconApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vmscon/vmscon/VmsconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmscon.vmscon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmsconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
