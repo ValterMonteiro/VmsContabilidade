4764eab8f297c46e077ad098fb9a96ef17a0d06e
ValterMonteiro <valtinms@gmail.com>
2021-09-07 21:23:06 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className = "text-primary"> Olá! Bem vindo a VMS Contabilidade!👨‍💻</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary"> Olá! Bem vindo a VMS Contabilidade!👨‍💻</h1>
+
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://vmscon.com/" target="_blank" rel="noreferrer">Valter Monteiro</a></p>
+                <p className="text-light"><small><strong>VMS Contabilidade</strong><br />
+                    App VMS Contabilidade: <a href="informar instagram" target="_blank" rel="noreferrer">@intagram</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
